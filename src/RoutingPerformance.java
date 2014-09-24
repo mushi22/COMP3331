c61f27ac4838f2e28b06568c7a81ff4b4c8cb644
Rashim <ahsan.rahman.92@gmail.com>
2014-09-24 17:07:23 +1000

    readfiles, and arguments passing

--- src/RoutingPerformance.java
+++ src/RoutingPerformance.java
@@ -1,5 +1,9 @@
+import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
 
 
 public class RoutingPerformance {
@@ -9,15 +13,31 @@ public class RoutingPerformance {
 	
 	
 	private static int packetRate;
-	public static void main(String[] args) throws FileNotFoundException {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br;
+		String  line;
 		
 		networkScheme = args[0];
 		routingScheme = args[1];
 		FileInputStream tFile = new FileInputStream(args[2]);
+		
 		FileInputStream wFile = new FileInputStream(args[3]);
 		
 		packetRate = Integer.parseInt(args[4]);
-
+        
+		
+		System.out.println("network scheme:"+ " " +networkScheme);
+		System.out.println("routing scheme:"+ " " +routingScheme);
+		
+		br = new BufferedReader(new InputStreamReader(tFile, Charset.forName("UTF-8")));
+		while ((line = br.readLine()) != null) {
+		    System.out.println(line);
+		}
+		br = new BufferedReader(new InputStreamReader(wFile, Charset.forName("UTF-8")));
+		while ((line = br.readLine()) != null) {
+		    System.out.println(line);
+		}
+		System.out.println("packet rate:"+ " " +packetRate);
 	}
 	//adding comments
 

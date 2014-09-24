4ed46309e64bc8e045bd50ccbb450eb1a74d142a
Rashim <ahsan.rahman.92@gmail.com>
2014-09-24 16:23:33 +1000

    new main

--- src/RoutingPerformance.java
+++ src/RoutingPerformance.java
@@ -1,4 +1,18 @@
 
 public class RoutingPerformance {
+    
+	private static String networkScheme = null;
+	private static String routingScheme = null;
+	
+	
+	private static int packetRate;
+	public static void main(String[] args) {
+		
+		networkScheme = args[0];
+		routingScheme = args[1];
+		
+		packetRate = Integer.parseInt(args[4]);
+
+	}
 
 }

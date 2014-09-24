600534cef3ce7a62ec3fda430c0a379e3df13170
Rashim <ahsan.rahman.92@gmail.com>
2014-09-24 17:27:33 +1000

    added graph stuff

--- src/RoutingPerformance.java
+++ src/RoutingPerformance.java
@@ -4,6 +4,7 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.Charset;
+import java.util.ArrayList;
 
 
 public class RoutingPerformance {
@@ -38,6 +39,54 @@ public class RoutingPerformance {
 		    System.out.println(line);
 		}
 		System.out.println("packet rate:"+ " " +packetRate);
+		
+		GraphImple graph = new GraphImple();
+		Graphnode a = new Graphnode("a");
+		Graphnode b = new Graphnode("b");
+		Graphnode c = new Graphnode("c");
+		Graphnode d = new Graphnode("d");
+		Graphnode e = new Graphnode("e");
+		Graphnode f = new Graphnode("f");
+		Graphnode g = new Graphnode("g");
+		Graphnode h = new Graphnode("h");
+		Graphnode i = new Graphnode("i");
+		Graphnode j = new Graphnode("j");
+		Graphnode k = new Graphnode("k");
+		
+		graph.addNode(a);
+		graph.addNode(b);
+		graph.addNode(c);
+		graph.addNode(d);
+		graph.addNode(e);
+		graph.addNode(f);
+		graph.addNode(g);
+		graph.addNode(h);
+		graph.addNode(i);
+		graph.addNode(j);
+		graph.addNode(k);
+		
+		graph.addNonDEdge(a, b);
+		graph.addNonDEdge(a, d);
+		graph.addNonDEdge(b, c);
+		graph.addNonDEdge(c, d);
+		graph.addNonDEdge(d, g);
+		graph.addNonDEdge(d, e);
+		graph.addNonDEdge(e, f);
+		graph.addNonDEdge(f, g);
+		graph.addNonDEdge(f, j);
+		graph.addNonDEdge(g, j);
+		graph.addNonDEdge(g, h);
+		graph.addNonDEdge(j, i);
+		graph.addNonDEdge(i, k);
+		
+		ArrayList<Graphnode> path = graph.bfs(a, k);
+		printList(path);
+
+	}
+	public static void printList(ArrayList<Graphnode> list){
+		for(Graphnode n : list){
+			System.out.print("[" + n.getName() + "]");
+		}
 	}
 	//adding comments
 
